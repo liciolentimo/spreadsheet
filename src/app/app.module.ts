80bef0ed0fe2e775431827eb8c55c7719325f079
liciolentimo <lentimo@gmail.com>
2021-02-10 18:52:29 +0300

    replaced with mat-table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,92 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+
+// @NgModule({
+//   declarations: [
+//     AppComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule,
+//     BrowserAnimationsModule,
+    
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+
+
+// export class AppModule { }
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {CommonModule} from '@angular/common';
+import {CdkTableModule} from '@angular/cdk/table';
+import { FormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatTreeModule} from '@angular/material/tree';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatIconModule} from '@angular/material/icon';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatInputModule} from '@angular/material/input';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatTableModule} from '@angular/material/table';
+import {MatListModule} from '@angular/material/list';
+import {MatSelectModule} from '@angular/material/select';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatPaginatorModule} from '@angular/material/paginator';
+
+
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    FormsModule,
+    CdkTableModule,
+    NoopAnimationsModule,
+    CommonModule,
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    
+    MatButtonModule,
+    MatCardModule,
+    MatMenuModule,
+    MatTreeModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatStepperModule,
+    MatIconModule,
+    MatRadioModule,
+    MatDialogModule,
+    MatChipsModule,
+    MatInputModule,
+    MatTooltipModule,
+    MatTableModule,
+    MatListModule,
+    MatSelectModule,
+    MatExpansionModule,
+    MatAutocompleteModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-
-
 export class AppModule { }
